dd7c82b573d8156ec5d730600bbc5444b4ff03fe
Antonio Revert Hinojosa <a.revert.hinojosa@gmail.com>
2016-10-01 19:27:23 +0200

    Dropzone json communication tested for /utils/upload.php with upload and remove functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Shop_v1.2
+
+V1.2 adding new features such a dropzone, and taking out php code from the form for new products
--- utils/upload.php
+++ utils/upload.php
@@ -2,11 +2,11 @@
 //echo json_encode("Hola mundo desde upload.php ");
 
 function upload_files() {
-echo json_encode("Hola mundo desde upload.php ");
+echo json_encode("Hello world from upload_files in upload.php ");
 exit;
 }
 
 function remove_files(){
-  echo json_encode("Hola mundo desde remove en upload.php");
+  echo json_encode("Hello world from remove in upload.php");
   exit;
 }
